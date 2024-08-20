849deaa948cee2c60ced8e5f08880cffe96c9237
Anthony Burns <anthonyalanb@hotmail.com>
2024-08-20 22:29:48 +0000

    Getting rid of useNewUrlParser

--- db-connection.js
+++ db-connection.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const uri = process.env.MONGO_URI;
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(uri)
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
 
