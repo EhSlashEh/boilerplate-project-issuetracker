5509de24b7c8f7645ca9c1eb6deb029438f346ff
Anthony Burns <anthonyalanb@hotmail.com>
2024-08-20 16:28:56 +0000

    Password doesnt need brackets

--- db-connection.js
+++ db-connection.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const uri = process.env.MONGO_URI;
 
-const db = mongoose.connect(uri);
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
 
-module.exports = db;
+module.exports = mongoose;
